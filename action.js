024f5ec345de413a1188b9dc75d7fa24b183d9f1
mtfoley <mtfoley.mae@gmail.com>
2022-01-16 23:26:46 -0500

    debug payload temporarily

--- action.js
+++ action.js
@@ -6,10 +6,7 @@ async function run(){
 //    const token = core.getInput("token");
 //    const octokit = github.getOctokit(token);
     const context = github.context;
-    const title = context.payload.pull_request.title;
-    const {login} = context.payload.pull_request.author;
-    const {owner,name} = context.payload.repository;
-    core.debug(JSON.stringify({title,login,owner,name}));
+    core.debug(JSON.stringify(context.payload));
   } catch (error) {
     core.setFailed(error.message);
   }  
