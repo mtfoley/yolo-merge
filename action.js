17724388a2bd45ab2dc5a2065740bf564415b0cc
mtfoley <mtfoley.mae@gmail.com>
2022-01-16 23:38:27 -0500

    debug mergeable

--- action.js
+++ action.js
@@ -7,9 +7,9 @@ async function run(){
 //    const octokit = github.getOctokit(token);
     const context = github.context;
     const title = context.payload.pull_request.title;
-    const {login} = context.payload.pull_request.user;
+    const {mergeable,mergeable_state,user:{login}} = context.payload.pull_request;
     const {owner:{login:repoOwner},name:repoName} = context.payload.repository;
-    core.debug(JSON.stringify({title,login,repoOwner,repoName}));
+    core.debug(JSON.stringify({title,login,repoOwner,repoName,mergeable,mergeable_state}));
   } catch (error) {
     core.setFailed(error.message);
   }  
